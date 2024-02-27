d5bf54e64b6030432aac71a984c311afc00a048f
Marek Mytkowski <marek.mytkowski.mm@gmail.com>
2024-02-27 20:26:39 +0100

    Add auto migration script to Program.cs

--- InGreedIoApi/Program.cs
+++ InGreedIoApi/Program.cs
@@ -14,6 +14,18 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Run migrations
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ApiDbContext>();
+    if (context.Database.GetPendingMigrations().Any())
+    {
+        context.Database.Migrate();
+    }
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
