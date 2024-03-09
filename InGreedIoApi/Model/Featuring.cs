c749d327b53302a1fa557f54b9a3e82de78f0d28
bmajosek <bartek9568@gmail.com>
2024-03-09 15:53:51 +0100

    add models

---
+++ InGreedIoApi/Model/Featuring.cs
@@ -0,0 +1,11 @@
+﻿namespace InGreedIoApi.Model
+{
+    public class Featuring
+    {
+        public int Id { get; set; }
+        public DateTime? ExpireDate { get; set; }
+        public bool PaymentConfirmed { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
---
+++ InGreedIoApi/Model/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace InGreedIoApi.Model
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public float Rating { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
