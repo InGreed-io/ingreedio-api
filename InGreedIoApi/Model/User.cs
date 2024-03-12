29e1cfaf50b165395e6ec8c0355d85d7a08d1d1a
bmajosek <bartek9568@gmail.com>
2024-03-12 18:57:54 +0100

    company nullable

--- InGreedIoApi/Model/User.cs
+++ InGreedIoApi/Model/User.cs
@@ -6,7 +6,7 @@
         public string Mail { get; set; }
         public string Username { get; set; }
         public bool IsBlocked { get; set; }
-        public int CompanyId { get; set; }
+        public int? CompanyId { get; set; }
         public CompanyInfo? Company { get; set; }
         public ICollection<Product> FavouriteProducts { get; set; }
     }
