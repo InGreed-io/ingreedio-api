247fe76b7eb6c601da9f12825e81b8293c7de54a
bmajosek <bartek9568@gmail.com>
2024-03-12 18:55:33 +0100

    companyinfo can be null

--- InGreedIoApi/Model/User.cs
+++ InGreedIoApi/Model/User.cs
@@ -7,7 +7,7 @@
         public string Username { get; set; }
         public bool IsBlocked { get; set; }
         public int CompanyId { get; set; }
-        public CompanyInfo Company { get; set; }
+        public CompanyInfo? Company { get; set; }
         public ICollection<Product> FavouriteProducts { get; set; }
     }
 }
