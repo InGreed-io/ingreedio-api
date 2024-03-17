ef44bda14a8f03bae0e980cc87064bdc89bf8580
bmajosek <bartek9568@gmail.com>
2024-03-17 13:59:57 +0100

    add-mapper

---
+++ InGreedIoApi/Data/Mapper/POCOMapper.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using InGreedIoApi.Model;
+using InGreedIoApi.POCO;
+
+namespace InGreedIoApi.Data.Mapper
+{
+    public class POCOMapper : Profile
+    {
+        public POCOMapper()
+        {
+            // From POCO to Models
+            CreateMap<CategoryPOCO, Category>();
+            CreateMap<CompanyInfoPOCO, CompanyInfo>();
+            CreateMap<FeaturingPOCO, Featuring>();
+            CreateMap<IngredientPOCO, Ingredient>();
+            CreateMap<ReviewPOCO, Review>();
+            CreateMap<ProductPOCO, Product>();
+
+            // From Models to POCO
+            CreateMap<Category, CategoryPOCO>();
+            CreateMap<CompanyInfo, CompanyInfoPOCO>();
+            CreateMap<Featuring, FeaturingPOCO>();
+            CreateMap<Ingredient, IngredientPOCO>();
+            CreateMap<Review, ReviewPOCO>();
+            CreateMap<Product, ProductPOCO>();
+        }
+    }
+}
