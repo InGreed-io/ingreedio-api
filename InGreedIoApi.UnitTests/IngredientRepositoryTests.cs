f68e823757ad6485d0d19c83677b9c39d223bb2f
maj bartosz <majb@p21802.MINI>
2024-04-08 15:21:27 +0200

    add ingredients repository tests

---
+++ InGreedIoApi.UnitTests/IngredientRepositoryTests.cs
@@ -0,0 +1,57 @@
+
+using AutoMapper;
+using InGreedIoApi.Data.Repository;
+using InGreedIoApi.Data;
+using InGreedIoApi.POCO;
+using Microsoft.EntityFrameworkCore;
+using InGreedIoApi.Data.Mapper;
+
+namespace UnitTests;
+
+public class IngredientRepositoryTests
+{
+    private readonly ApiDbContext _mockContext;
+    private readonly IMapper _mockMapper;
+    private readonly List<IngredientPOCO> _ingredients;
+
+    public IngredientRepositoryTests()
+    {
+        var options = new DbContextOptionsBuilder<ApiDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        _mockContext = new ApiDbContext(options);
+
+        var configuration = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<POCOMapper>();
+        });
+        _mockMapper = configuration.CreateMapper();
+
+        _ingredients = new List<IngredientPOCO>
+        {
+            new IngredientPOCO { Id = 1, Name = "Ingredient 1", IconUrl = "Icon 1" },
+            new IngredientPOCO { Id = 2, Name = "Ingredient 2", IconUrl = "Icon 2"  }
+        };
+
+        _mockContext.Ingredients.AddRange(_ingredients);
+        _mockContext.SaveChanges();
+    }
+
+    [Trait("Category", "Unit")]
+    [Fact]
+    public async Task FindAll_ReturnsCorrectIngredients_ReturnFirstPageOfSizeOne()
+    {
+        // Arrange
+        var repository = new IngredientRepository(_mockMapper, _mockContext);
+        var query = "Ingred";
+        int page = 0, limit = 1;
+        // Act
+        var ingredients = await repository.FindAll(query, page, limit);
+
+        // Assert
+        Assert.NotNull(ingredients);
+        Assert.Equal(limit, ingredients.Count());
+        Assert.Equal("Ingredient 1", ingredients.First().Name);
+    }
+}
