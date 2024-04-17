a52c2f27d5739492451d9ca2d1b24d84f18c5cc2
bmajosek <bartek9568@gmail.com>
2024-04-17 11:50:23 +0200

    remove from record attributes

--- InGreedIoApi/DTO/GetIngredientsDTO.cs
+++ InGreedIoApi/DTO/GetIngredientsDTO.cs
@@ -2,15 +2,5 @@
 
 namespace InGreedIoApi.DTO
 {
-    public record GetIngredientsQuery
-    {
-        [FromQuery(Name = "Query")]
-        public string Query { get; init; }
-
-        [FromQuery(Name = "Page")]
-        public int Page { get; init; }
-
-        [FromQuery(Name = "Limit")]
-        public int Limit { get; init; }
-    }
+    public record GetIngredientsQuery(string Query, int Page, int Limit);
 }
