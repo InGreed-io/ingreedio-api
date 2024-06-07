31d6370392888301cdedeb242164689d08524b09
Willu12 <boryskurdek@gmail.com>
2024-06-07 16:36:13 +0200

    fixed filtering products by wanted ingredients

--- InGreedIoApi/Services/ProductService.cs
+++ InGreedIoApi/Services/ProductService.cs
@@ -6,7 +6,7 @@ namespace InGreedIoApi.Services;
 
 public class ProductService : IProductService
 {
-    public void SortProductQueryDto(ProductQueryDTO productQueryDto, ref IQueryable<ProductPOCO> queryable)
+    public void SortProductQueryDto(ProductQueryDTO productQueryDto, ref IQueryable<ProductPOCO> queryable, IEnumerable<int> wantedProducts)
     {
         if (productQueryDto.SortBy.HasValue)
         {
@@ -16,7 +16,9 @@ public class ProductService : IProductService
                 QuerySortType.Rating => queryable.OrderBy(p => p.Reviews.Average(r => r.Rating) == null)
                   .ThenByDescending(p => p.Reviews.Average(r => r.Rating)).ThenBy(p => p.Id),
                 QuerySortType.RatingCount => queryable.OrderByDescending(p => p.Reviews.Count()).ThenBy(p => p.Id),
-                QuerySortType.BestMatch => queryable.OrderBy(p => p.Id),
+                QuerySortType.BestMatch => queryable.OrderBy(p => p.Ingredients.Select(p => p.Id).Union(wantedProducts).Count())
+                .ThenByDescending(p => p.Ingredients.Select(p => p.Id).Except(wantedProducts).Count())
+                .ThenBy(p => p.Id),
                 QuerySortType.Names => queryable.OrderBy(p => p.Name).ThenBy(p => p.Id),
                 _ => throw new ArgumentOutOfRangeException("sorty is not defined properly")
             };
